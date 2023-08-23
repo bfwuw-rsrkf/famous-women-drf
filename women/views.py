437fb45d6d3d9974f17b79f02d2479beb750a560
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-23 15:43:40 +0600

    added WomanListAPIView

--- women/views.py
+++ women/views.py
@@ -1,8 +1,24 @@
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
 from .models import Woman
 from .serializers import WomanSerializer
 
 
-class WomanListAPIView(generics.ListAPIView):
-    queryset = Woman.objects.all()
-    serializer_class = WomanSerializer
+class WomanListAPIView(APIView):
+    def get(self, request):
+        posts = Woman.objects.all().values()
+        return Response({'posts': posts})
+
+    def post(self, request):
+        post_new = Woman.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            is_published=request.data['is_published'],
+        )
+        return Response({'post': post_new})
+
+
+# class WomanListAPIView(generics.ListAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
