64252aa7dcd73dcfa8a86213e87fcd1246d92e8f
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-24 16:45:50 +0600

    added get, post and put methods in WomanListAPIView inherited from APIView

--- women/views.py
+++ women/views.py
@@ -1,5 +1,6 @@
+from django.forms import model_to_dict
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView
+# from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from .models import Woman
 from .serializers import WomanSerializer
@@ -7,16 +8,27 @@ from .serializers import WomanSerializer
 
 class WomanListAPIView(APIView):
     def get(self, request):
-        posts = Woman.objects.all().values()
-        return Response({'posts': posts})
+        posts = Woman.objects.all()
+        return Response({'posts': WomanSerializer(posts, many=True).data})
 
     def post(self, request):
-        post_new = Woman.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            is_published=request.data['is_published'],
-        )
-        return Response({'post': post_new})
+        serializer = WomanSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'post': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'Method Put not allowed.'})
+        try:
+            instance = Woman.objects.get(pk=pk)
+        except KeyError:
+            return Response({'error': 'Object does not exist.'})
+        serializer = WomanSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'post': serializer.data})
 
 
 # class WomanListAPIView(generics.ListAPIView):
