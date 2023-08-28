ff5c36a688ae89d401bf842442f135e7057d208f
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-28 16:14:49 +0600

    new endpoints for corresponding views

--- women/urls.py
+++ women/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('woman-list/', WomanListAPIView.as_view(), name='woman-list'),
-    path('woman-list/<int:pk>', WomanListAPIView.as_view(), name='woman-list')
+    path('woman-create/', WomanCreateAPIView.as_view(), name='woman-post'),
+    path('woman-list/', WomanListAPIView.as_view(), name='woman-get'),
+    path('woman-update/<int:pk>/', WomanUpdateAPIView.as_view(), name='woman-put'),
+    path('woman-destroy/<int:pk>/', WomanDestroyAPIView.as_view(), name='woman-delete')
 ]
