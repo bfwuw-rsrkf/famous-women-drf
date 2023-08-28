2163c85a7660b5f1796c37bbec268f40867335f4
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-28 22:35:33 +0600

    now using two hybrid APIViews

--- women/views.py
+++ women/views.py
@@ -1,23 +1,38 @@
-from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView
+# from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Woman
+from .permissions import IsOwnerOrReadOnly
 from .serializers import WomanSerializer
 
 
-class WomanCreateAPIView(CreateAPIView):
+# class WomanCreateAPIView(CreateAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
+#
+#
+# class WomanListAPIView(ListAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
+#
+#
+# class WomanUpdateAPIView(UpdateAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
+#
+#
+# class WomanDestroyAPIView(DestroyAPIView):
+#     queryset = Woman.objects.all()
+#     serializer_class = WomanSerializer
+
+
+class WomanListCreateAPIView(ListCreateAPIView):
     queryset = Woman.objects.all()
     serializer_class = WomanSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
-class WomanListAPIView(ListAPIView):
+class WomanRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Woman.objects.all()
     serializer_class = WomanSerializer
-
-
-class WomanUpdateAPIView(UpdateAPIView):
-    queryset = Woman.objects.all()
-    serializer_class = WomanSerializer
-
-
-class WomanDestroyAPIView(DestroyAPIView):
-    queryset = Woman.objects.all()
-    serializer_class = WomanSerializer
+    permission_classes = (IsOwnerOrReadOnly,)
