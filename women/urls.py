abea8acae37836399559f95de3b24eab770fc3c1
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-28 22:35:15 +0600

    added authorization and now using two hybrid APIView

--- women/urls.py
+++ women/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('woman-create/', WomanCreateAPIView.as_view(), name='woman-post'),
-    path('woman-list/', WomanListAPIView.as_view(), name='woman-get'),
-    path('woman-update/<int:pk>/', WomanUpdateAPIView.as_view(), name='woman-put'),
-    path('woman-destroy/<int:pk>/', WomanDestroyAPIView.as_view(), name='woman-delete')
+    path('auth/', include('rest_framework.urls')),
+    # path('woman-create/', WomanCreateAPIView.as_view(), name='woman-post'),
+    # path('woman-list/', WomanListAPIView.as_view(), name='woman-get'),
+    # path('woman-update/<int:pk>/', WomanUpdateAPIView.as_view(), name='woman-put'),
+    # path('woman-destroy/<int:pk>/', WomanDestroyAPIView.as_view(), name='woman-delete'),
+    path('woman-list/', WomanListCreateAPIView.as_view(), name='woman-list-create'),
+    path('woman-list/<int:pk>/', WomanRetrieveUpdateDestroyView.as_view(), name='woman-retr-upd-destr')
 ]
