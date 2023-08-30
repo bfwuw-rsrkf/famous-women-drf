fc2affd42c52b2dd67ee00957038b3ab03c4ee04
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-30 16:13:06 +0600

    added paths for djoser

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('women/', include('women.urls'))
+    path('women/', include('women.urls')),
+    re_path(r'^auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
