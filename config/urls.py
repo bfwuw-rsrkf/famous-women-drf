5a5569329bc28d627fb3e5634f86cf4c4845e1ee
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-30 17:18:44 +0600

    deleted unnecessary comments

--- config/urls.py
+++ config/urls.py
@@ -8,9 +8,3 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-# refresh
-# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY5MzQ3ODE4NywiaWF0IjoxNjkzMzkxNzg3LCJqdGkiOiIyYTZkZTBiMTU3YzI0NDMwYWQzOThlZGUyMGExYzU3YiIsInVzZXJfaWQiOjJ9.n5eo2Nv3I_LnklINQQGkIg6C12Sz9TBY1SWqq70C-_4
-
-# access
-# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkzMzkyMDg3LCJpYXQiOjE2OTMzOTE3ODcsImp0aSI6ImMyMjAyNzRiNWM2MjRkNWQ5YTgwMTA5YjIzMTljMjNkIiwidXNlcl9pZCI6Mn0.WRxednpyITS3JiR6kkDqycVAp3viRJeMZJdBGgn5GFk
