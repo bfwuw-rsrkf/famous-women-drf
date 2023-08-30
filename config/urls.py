a9abc2843d548989c647f38deadf08894c376dd7
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-30 17:17:42 +0600

    added pathst for jwt tokens

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('women/', include('women.urls')),
-    re_path(r'^auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+# refresh
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY5MzQ3ODE4NywiaWF0IjoxNjkzMzkxNzg3LCJqdGkiOiIyYTZkZTBiMTU3YzI0NDMwYWQzOThlZGUyMGExYzU3YiIsInVzZXJfaWQiOjJ9.n5eo2Nv3I_LnklINQQGkIg6C12Sz9TBY1SWqq70C-_4
+
+# access
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkzMzkyMDg3LCJpYXQiOjE2OTMzOTE3ODcsImp0aSI6ImMyMjAyNzRiNWM2MjRkNWQ5YTgwMTA5YjIzMTljMjNkIiwidXNlcl9pZCI6Mn0.WRxednpyITS3JiR6kkDqycVAp3viRJeMZJdBGgn5GFk
