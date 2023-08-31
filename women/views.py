d4a65e403e16b846c9e1056644f0f55061d61e22
bfwuw-rsrkf <zy.130606@gmail.com>
2023-08-31 15:43:45 +0600

    added pagination for WomanListCreateAPIView

--- women/views.py
+++ women/views.py
@@ -2,6 +2,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Woman
+from .pagination import PaginationWoman
 from .permissions import IsOwnerOrReadOnly
 from .serializers import WomanSerializer
 
@@ -29,10 +30,12 @@ from .serializers import WomanSerializer
 class WomanListCreateAPIView(ListCreateAPIView):
     queryset = Woman.objects.all()
     serializer_class = WomanSerializer
+    pagination_class = PaginationWoman
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class WomanRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Woman.objects.all()
     serializer_class = WomanSerializer
+    pagination_class = PaginationWoman
     permission_classes = (IsOwnerOrReadOnly,)
